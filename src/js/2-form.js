e03d321e210ff8341928d901b89eb1c10de79b08
MaksymSobotiuk <maxim.4.july.2001@gmail.com>
2024-03-05 20:56:03 +0200

    form-done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,58 @@
-console.log("Form")
+
+const storageKey = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+const formEmail= form.email;
+const formMessage = form.message;
+
+function readFormData() {
+    const email = formEmail.value.trim();
+    const message = formMessage.value.trim();
+
+    return {
+        email,
+        message,
+        };
+}
+
+function saveFormDataToLocalStorage(data) {
+    const jsonData = JSON.stringify(data);
+    localStorage.setItem(storageKey, jsonData);
+}
+
+function populateFormFromLocalStorage() {
+    const rawData = localStorage.getItem(storageKey);
+    if (rawData) {
+        const data = JSON.parse(rawData);
+
+        if (data.email !== undefined) {
+            formEmail.value = data.email;
+        }
+
+        if (data.message !== undefined) {
+            formMessage.value = data.message;
+        }
+        }
+    }
+
+populateFormFromLocalStorage();
+
+form.addEventListener("input", () => {
+    const data = readFormData();
+    saveFormDataToLocalStorage(data);
+});
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const data = readFormData();
+
+        if (data.email && data.message) {
+            console.log(data);
+
+            localStorage.removeItem(storageKey);
+            formEmail.value = "";
+            formMessage.value = "";
+        } else {
+            alert("Please fill in both email and message fields.");
+        }
+    });
